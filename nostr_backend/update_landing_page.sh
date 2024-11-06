1f0795d3b98d10bced66d6cd8165940dd6bfe29f
Twenty Eighty <twenty-eighty@xn--y9a.net>
2024-11-06 06:39:25 +0100

    fix LP after downloading

--- nostr_backend/update_landing_page.sh
+++ nostr_backend/update_landing_page.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-curl --compressed "https://pareto.onepage.me" > priv/static/lp/en/index.html
+INDEX_EN=priv/static/lp/en/index.html
+INDEX_DE=priv/static/lp/de/index.html
+
+curl --compressed "https://pareto.onepage.me" > $INDEX_EN
+
+sed -i -e 's|https://pareto.onepage.me|https://pareto.space|g' -e 's|content="pareto.onepage.me"|content="The Pareto Project"|g' -e 's|lang="de"|lang="en"|g' $INDEX_EN
+
+curl --compressed "https://pareto.onepage.me" > $INDEX_DE
+
+sed -i -e 's|https://pareto.onepage.me|https://pareto.space|g' -e 's|content="pareto.onepage.me"|content="Das Pareto Projekt"|g' $INDEX_DE
 
