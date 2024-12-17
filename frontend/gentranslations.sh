8aa18cb259a80a23aea5253a53499dbbb5e36813
twenty-eighty <github.com-pareto@xn--y9a.net>
2024-12-17 10:06:07 +0100

    made run on macOS

--- frontend/gentranslations.sh
+++ frontend/gentranslations.sh
@@ -8,6 +8,13 @@ $ELMI18NEXTGEN --output=gen/Translations --translations=lang/lang-en_US.json
 
 CONFLICTING_FILE=gen/Translations/Language.elm
 
-sed -i '1s/module Language/module DefaultLanguage/' "$CONFLICTING_FILE"
+# Check if the sed version supports --version (GNU sed)
+if sed --version >/dev/null 2>&1; then
+  # GNU sed detected
+  sed -i '1s/module Language/module DefaultLanguage/' "$CONFLICTING_FILE"
+else
+  # Assume BSD sed (macOS or other systems)
+  sed -i '' '1s/module Language/module DefaultLanguage/' "$CONFLICTING_FILE"
+fi
 
 mv $CONFLICTING_FILE gen/Translations/DefaultLanguage.elm
