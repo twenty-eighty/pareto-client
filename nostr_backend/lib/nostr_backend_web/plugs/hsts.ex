153ee300e5ea5e8af0be5a38738ada9adf205c50
Twenty Eighty <twenty-eighty@xn--y9a.net>
2024-12-23 13:06:31 +0100

    set strict-transport-security (HSTS) HTTP header

---
+++ nostr_backend/lib/nostr_backend_web/plugs/hsts.ex
@@ -0,0 +1,9 @@
+defmodule NostrBackendWeb.Plugs.Hsts do
+  import Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    put_resp_header(conn, "strict-transport-security", "max-age=31536000; includeSubDomains")
+  end
+end
