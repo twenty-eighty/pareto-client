a59400a52547186c5faaf817c8234d648b0fd799
Twenty Eighty <twenty-eighty@xn--y9a.net>
2025-01-14 16:29:38 +0000

    landing page update

--- nostr_backend/lib/nostr_backend_web/router.ex
+++ nostr_backend/lib/nostr_backend_web/router.ex
@@ -50,6 +50,7 @@ defmodule NostrBackendWeb.Router do
     get("/notifications", PageController, :notifications)
     get("/posts", PageController, :posts)
     get("/read", PageController, :read)
+    get("/settings", PageController, :settings)
     get("/sign-in", PageController, :sign_in)
     get("/write", PageController, :write)
 
