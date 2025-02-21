808743406e1c993a23114f4c05a6291099747fb9
Twenty Eighty <twenty-eighty@xn--y9a.net>
2025-02-21 17:49:08 +0000

    build lnurlp data dynamically

--- nostr_backend/lib/nostr_backend_web/controllers/lightning_controller.ex
+++ nostr_backend/lib/nostr_backend_web/controllers/lightning_controller.ex
@@ -6,69 +6,41 @@ defmodule NostrBackendWeb.LightningController do
   @lnurlp_data %{
     "roland" => %{
       "nostrPubkey" => "79f00d3f5a19ec806189fcab03c1be4ff81d18ee4f653c88fac41fe03570f432",
-      "callback" => "https://getalby.com/lnurlp/rohe/callback",
-      "metadata" => "[[\"text/identifier\",\"rohe@getalby.com\"],[\"text/plain\",\"Sats for Separate money from state!\"]]",
-      "minSendable" => 1000,
-      "maxSendable" => 10000000000
+      "lud16" => "rohe@getalby.com"
     },
     "rodant" => %{
       "nostrPubkey" => "71df211931d26ee41121d295bd43cbc7e382505e333b5c13d4016ced9542d9d7",
-      "callback" => "https://getalby.com/lnurlp/vntonior7z/callback",
-      "metadata" => "[[\"text/identifier\",\"vntonior7z@getalby.com\"],[\"text/plain\",\"Sats for VntonIO\"]]",
-      "minSendable" => 1000,
-      "maxSendable" => 10000000000
+      "lud16" => "vntonior7z@getalby.com"
     },
     "janosch" => %{
       "nostrPubkey" => "89bae92f9d9b0f6d97a300496cfb0b73c92a74c9675a724c0689975f8074dc01",
-      "callback" => "https://livingroomofsatoshi.com/api/v1/lnurl/payreq/4845dac6-5b78-439d-9ab1-90e18bacbef6",
-      "metadata" => "[[\"text/plain\",\"Pay to Wallet of Satoshi user: restfulbench94\"],[\"text/identifier\",\"restfulbench94@walletofsatoshi.com\"]]",
-      "minSendable" => 1000,
-      "maxSendable" => 10000000000
+      "lud16" => "restfulbench94@walletofsatoshi.com"
     },
     "nachteule" => %{
       "nostrPubkey" => "9c8096eb84d574ca29eb0077d615a2b12c0113064faeac9f72e464a066e47555",
-      "callback" => "https://getalby.com/lnurlp/brokendarkness103/callback",
-      "metadata" => "[[\"text/identifier\",\"brokendarkness103@getalby.com\"],[\"text/plain\",\"Sats for Nachteule\"]]",
-      "minSendable" => 1000,
-      "maxSendable" => 10000000000
+      "lud16" => "brokendarkness103@getalby.com"
     },
     "kalle" => %{
       "nostrPubkey" => "08d79c2e514edd4634ea92bbfe1ec089730a049216be9d28c77c4c1c7733f518",
-      "callback" => "https://livingroomofsatoshi.com/api/v1/lnurl/payreq/4ad194e0-2904-43c4-b6ea-295daeb7ca28",
-      "metadata" => "[[\"text/plain\",\"Pay to Wallet of Satoshi user: balmyravioli90\"],[\"text/identifier\",\"balmyravioli90@walletofsatoshi.com\"]]",
-      "minSendable" => 1000,
-      "maxSendable" => 100000000000
+      "lud16" => "balmyravioli90@walletofsatoshi.com"
     },
     "donjoe" => %{
       "nostrPubkey" => "0f4795bf31824a414148daf1b589bb8138fb0a03963f984c84462e40a8365abe",
-      "callback" => "https://livingroomofsatoshi.com/api/v1/lnurl/payreq/5b03fa9a-7ab1-4bd1-bcfe-b2718049096c",
-      "metadata" => "[[\"text/plain\",\"Pay to Wallet of Satoshi user: compacthook86\"],[\"text/identifier\",\"compacthook86@walletofsatoshi.com\"]]",
-      "minSendable" => 1000,
-      "maxSendable" => 100000000000
+      "lud16" => "compacthook86@walletofsatoshi.com"
     },
     "_" => %{
       "nostrPubkey" => "a81a69992a8b7fff092bb39a6a335181c16eb37948f55b90f3c5d09f3c502c84",
-      "callback" => "https://livingroomofsatoshi.com/api/v1/lnurl/payreq/e7681b38-746f-4694-9fae-f972e1345d01",
-      "metadata" => "[[\"text/plain\",\"Pay to Wallet of Satoshi user: donate2pareto\"],[\"text/identifier\",\"donate2pareto@walletofsatoshi.com\"]]",
-      "minSendable" => 1000,
-      "maxSendable" => 100000000000
+      "lud16" => "donate2pareto@walletofsatoshi.com"
     },
     "milosz" => %{
       "nostrPubkey" => "2c917bfcfe4f3777ccacb4c968d6a3e9266d39a22db65c2cf2ca0c09fddf8638",
-      "callback" => "https://livingroomofsatoshi.com/api/v1/lnurl/payreq/e7681b38-746f-4694-9fae-f972e1345d01",
-      "metadata" => "[[\"text/plain\",\"Pay to Wallet of Satoshi user: donate2pareto\"],[\"text/identifier\",\"donate2pareto@walletofsatoshi.com\"]]",
-      "minSendable" => 1000,
-      "maxSendable" => 100000000000
+      "lud16" => "donate2pareto@walletofsatoshi.com"
     },
     "ashoka" => %{
       "nostrPubkey" => "e373ca4101e25a4d4fcb2a53473fa4113b91dba2c2e451d039d8528eb82abcc5",
-      "callback" => "https://livingroomofsatoshi.com/api/v1/lnurl/payreq/e7681b38-746f-4694-9fae-f972e1345d01",
-      "metadata" => "[[\"text/plain\",\"Pay to Wallet of Satoshi user: donate2pareto\"],[\"text/identifier\",\"donate2pareto@walletofsatoshi.com\"]]",
-      "minSendable" => 1000,
-      "maxSendable" => 100000000000
+      "lud16" => "donate2pareto@walletofsatoshi.com"
     }
   }
-
 
   def lnurlp(conn, %{"username" => username}) do
     conn = put_required_headers(conn)
@@ -77,25 +49,45 @@ defmodule NostrBackendWeb.LightningController do
       nil ->
         conn
         |> put_status(:not_found)
-        |> text("LNURL-P data not found for username: #{username}")
+        |> json(%{error: "User not found"})
 
-      data ->
-        json(conn, %{
-          "status" => "OK",
-          "tag" => "payRequest",
-          "commentAllowed" => 255,
-          "callback" => data["callback"],
-          "metadata" => data["metadata"],
-          "minSendable" => data["minSendable"],
-          "maxSendable" => data["maxSendable"],
-          "payerData" => %{
-            "name" => %{"mandatory" => false},
-            "email" => %{"mandatory" => false},
-            "pubkey" => %{"mandatory" => false}
-          },
-          "nostrPubkey" => data["nostrPubkey"],
-          "allowsNostr" => true
-        })
+      %{"nostrPubkey" => nostr_pubkey, "lud16" => lud16} ->
+        lnurlp_url = build_lnurlp_url(lud16)
+
+        case fetch_lnurlp_json(lnurlp_url) do
+          {:ok, json_data} ->
+            updated_json = Map.put(json_data, "nostrPubkey", nostr_pubkey)
+
+            conn
+            |> put_status(:ok)
+            |> json(updated_json)
+
+          {:error, reason} ->
+            conn
+            |> put_status(:bad_gateway)
+            |> json(%{error: "Failed to fetch LNURLP data", reason: reason})
+        end
+    end
+  end
+
+  defp build_lnurlp_url(lud16) do
+    [username, domain] = String.split(lud16, "@")
+    "https://#{domain}/.well-known/lnurlp/#{username}"
+  end
+
+  defp fetch_lnurlp_json(url) do
+    case HTTPoison.get(url, [], follow_redirect: true) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        case Jason.decode(body) do
+          {:ok, json} -> {:ok, json}
+          {:error, _} -> {:error, "Invalid JSON response"}
+        end
+
+      {:ok, %HTTPoison.Response{status_code: code}} ->
+        {:error, "HTTP error: #{code}"}
+
+      {:error, reason} ->
+        {:error, reason}
     end
   end
 
