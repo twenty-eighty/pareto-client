e1c4abd81fb9bc0bcab92d041cf2504a028479cb
rodant <rodant68@gmail.com>
2025-03-06 18:06:24 +0100

    Read the tech info from an md file.

--- frontend/src/Pages/TechDetails.elm
+++ frontend/src/Pages/TechDetails.elm
@@ -1,22 +1,20 @@
 module Pages.TechDetails exposing (Model, Msg, page)
 
-import BrowserEnv exposing (BrowserEnv)
 import Effect exposing (Effect)
-import Html.Styled exposing (Html, a, div, h3, li, span, text, ul)
-import Html.Styled.Attributes exposing (css, href, target)
+import Html.Styled exposing (div, text)
+import Html.Styled.Attributes exposing (css)
+import Http
 import Layouts
-import Nostr.Event exposing (Kind, KindInformationLink(..), informationForKind, numberForKind)
-import Nostr.HandlerInformation exposing (WebTarget)
-import Nostr.Nips exposing (descriptionForNip)
+import Markdown
+import Nostr.Event exposing (KindInformationLink(..))
 import Page exposing (Page)
 import Pages.A.Addr_ exposing (toLayout)
-import Pareto
 import Route exposing (Route)
 import Shared
 import Tailwind.Utilities as Tw
 import Time exposing (Month(..))
 import Translations.TechDetails as Translations
-import Ui.Styles exposing (Styles, Theme, stylesForTheme)
+import Ui.Styles exposing (Theme)
 import View exposing (View)
 
 
@@ -42,12 +40,16 @@ toLayout theme _ =
 
 
 type alias Model =
-    {}
+    { techDetailsMarkdown : Maybe String }
 
 
 init : () -> ( Model, Effect Msg )
 init () =
-    ( {}, Effect.none )
+    ( { techDetailsMarkdown = Nothing }
+    , "/md/tech-details.md"
+        |> requestMarkdown
+        |> Effect.sendCmd
+    )
 
 
 
@@ -55,12 +57,19 @@ init () =
 
 
 type Msg
-    = NoOp
+    = ReceivedMarkdown (Result Http.Error String)
+    | NoOp
 
 
 update : Msg -> Model -> ( Model, Effect Msg )
 update msg model =
     case msg of
+        ReceivedMarkdown (Ok markdown) ->
+            ( { model | techDetailsMarkdown = Just markdown }, Effect.none )
+
+        ReceivedMarkdown (Err _) ->
+            ( model, Effect.none )
+
         NoOp ->
             ( model
             , Effect.none
@@ -81,142 +90,47 @@ subscriptions _ =
 
 
 view : Shared.Model -> Model -> View Msg
-view shared _ =
+view shared model =
     let
-        handlerInfo =
-            Pareto.applicationInformation shared.browserEnv.now
+        styles =
+            Ui.Styles.stylesForTheme shared.theme
     in
     { title = Translations.pageTitle [ shared.browserEnv.translations ]
     , body =
         [ div
-            [ css
-                [ Tw.m_4
-                , Tw.flex
-                , Tw.flex_col
-                , Tw.gap_4
-                ]
-            ]
-            [ viewWebTargets shared.theme shared.browserEnv handlerInfo.webTargets
-            , viewSupportedKinds shared.theme shared.browserEnv handlerInfo.kinds
-            , viewSupportedNips shared.theme shared.browserEnv Pareto.supportedNips
+            (styles.colorStyleBackground
+                ++ styles.colorStyleGrayscaleTitle
+                ++ [ css
+                        [ Tw.flex
+                        , Tw.flex_col
+                        , Tw.gap_3
+                        , Tw.m_10
+                        ]
+                   ]
+            )
+            [ model.techDetailsMarkdown
+                |> foldMaybe (div [] [])
+                    (\markdown ->
+                        case Markdown.markdownViewHtml styles Nothing (\_ -> Nothing) markdown of
+                            Ok html ->
+                                html
+
+                            Err error ->
+                                div [] [ text <| "Error rendering Markdown: " ++ error ]
+                    )
             ]
         ]
     }
 
 
-viewWebTargets : Theme -> BrowserEnv -> List WebTarget -> Html Msg
-viewWebTargets theme browserEnv webTargets =
-    let
-        styles =
-            Ui.Styles.stylesForTheme theme
-
-        viewWebTarget ( target, maybeType ) =
-            let
-                webTargetType =
-                    maybeType |> fold "" (\type_ -> " (" ++ type_ ++ ")")
-            in
-            li [] [ text <| target ++ webTargetType ]
-    in
-    div
-        []
-        [ h3 (styles.colorStyleGrayscaleTitle ++ styles.textStyleH3)
-            [ text <| Translations.supportedWebTargetsTitle [ browserEnv.translations ] ]
-        , ul [] (List.map viewWebTarget webTargets)
-        ]
+requestMarkdown : String -> Cmd Msg
+requestMarkdown url =
+    Http.get
+        { url = url
+        , expect = Http.expectString ReceivedMarkdown
+        }
 
 
-viewSupportedKinds : Theme -> BrowserEnv -> List Kind -> Html Msg
-viewSupportedKinds theme browserEnv kinds =
-    let
-        styles =
-            stylesForTheme theme
-
-        viewSupportedKind kind =
-            let
-                kindInfo =
-                    informationForKind kind
-            in
-            li []
-                [ text <| String.fromInt (numberForKind kind) ++ ": " ++ kindInfo.description ++ " ("
-                , viewKindLink styles kindInfo.link
-                , text ")"
-                ]
-    in
-    div
-        []
-        [ h3 (styles.colorStyleGrayscaleTitle ++ styles.textStyleH3)
-            [ text <| Translations.supportedKindsTitle [ browserEnv.translations ] ]
-        , ul [] (List.map viewSupportedKind kinds)
-        ]
-
-
-viewKindLink : Styles Msg -> Maybe KindInformationLink -> Html Msg
-viewKindLink styles link =
-    case link of
-        Just (LinkToNip nipNumber) ->
-            let
-                linkText =
-                    "NIP " ++ String.fromInt nipNumber
-            in
-            viewKindLink styles (Just <| OtherLink linkText (nipUrl nipNumber))
-
-        Just (LinkToNips nipNumbers) ->
-            nipNumbers
-                |> List.map (\nipNumber -> viewKindLink styles (Just <| LinkToNip nipNumber))
-                |> List.intersperse (span [] [ text ", " ])
-                |> span []
-
-        Just (OtherLink title url) ->
-            a (styles.textStyleLinks ++ styles.colorStyleArticleHashtags ++ [ href url, target "_blank" ])
-                [ text title ]
-
-        Nothing ->
-            span [] []
-
-
-viewSupportedNips : Theme -> BrowserEnv -> List String -> Html Msg
-viewSupportedNips theme browserEnv supportedNips =
-    let
-        styles =
-            Ui.Styles.stylesForTheme theme
-    in
-    div
-        [ css [ Tw.mt_3, Tw.mb_16 ] ]
-        [ h3 (styles.colorStyleGrayscaleTitle ++ styles.textStyleH3)
-            [ text <| Translations.supportedNipsTitle [ browserEnv.translations ] ]
-        , ul [] (List.map (viewNip theme) supportedNips)
-        ]
-
-
-viewNip : Theme -> String -> Html Msg
-viewNip theme nip =
-    let
-        styles =
-            Ui.Styles.stylesForTheme theme
-
-        nipInfoText =
-            descriptionForNip nip
-                |> Maybe.map (\description -> " (" ++ description ++ ")")
-                |> Maybe.withDefault ""
-
-        nipLink =
-            String.toInt nip |> fold ("https://nips.nostr.com/" ++ nip) nipUrl
-    in
-    li []
-        [ a
-            (styles.textStyleLinks
-                ++ styles.colorStyleArticleHashtags
-                ++ [ href nipLink, target "_blank" ]
-            )
-            [ text <| "NIP-" ++ nip ++ nipInfoText ]
-        ]
-
-
-nipUrl : Int -> String
-nipUrl nipNum =
-    "https://nips.nostr.com/" ++ String.fromInt nipNum
-
-
-fold : b -> (a -> b) -> Maybe a -> b
-fold default fn maybeA =
+foldMaybe : b -> (a -> b) -> Maybe a -> b
+foldMaybe default fn maybeA =
     maybeA |> Maybe.map fn |> Maybe.withDefault default
