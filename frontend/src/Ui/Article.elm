12577d7470fca4daf4b0a96b6ecabbd9bd734766
rodant <rodant68@gmail.com>
2025-04-30 12:02:48 +0200

    More layout improvements for the profile image.

--- frontend/src/Ui/Article.elm
+++ frontend/src/Ui/Article.elm
@@ -229,9 +229,7 @@ viewArticle articlePreviewsData articlePreviewData article =
                 , div
                     [ css
                         [ Tw.absolute
-                        , Bp.lg [ Tw.top_3over4 ]
-                        , Bp.md [ Tw.top_3over4 ]
-                        , Css.Media.withMediaQuery [ "(min-width: 360px)" ] [ Tw.top_1over2 ]
+                        , Tw.top_3over4
                         , Tw.z_10
                         ]
                     ]
