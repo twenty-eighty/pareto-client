f3466c3b84d51ff7d077810528e2e2039515cc46
Twenty Eighty <twenty-eighty@xn--y9a.net>
2026-09-14 19:15:08 -0500

    use .env vars for dev server

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ -f .env ]; then
+  set -a
+  # shellcheck disable=SC1091
+  source .env
+  set +a
+fi
+
 export ELM_HOME=elm-home/elm-stuff/
 
 node ./elm-kernel-replacements/run_replace-kernel-packages.mjs
